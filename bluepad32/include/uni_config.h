eb59689ff6b2c5558e1530f7f865376483fcfba3
jmunoz <juanmunoz890@gmail.com>
2023-10-02 21:25:43 -0300

    config

--- bluepad32/include/uni_config.h
+++ bluepad32/include/uni_config.h
@@ -21,6 +21,7 @@ limitations under the License.
 
 // For more configurations, please look at the Kconfig file, or just do:
 // "idf.py menuconfig" -> "Component config" -> "Bluepad32"
+#define UNI_ENABLE_BLE 1
 
 // How verbose the logging should be.
 // It is safe to leave UNI_LOG_DEBUG disabled (unless you are a developer).
